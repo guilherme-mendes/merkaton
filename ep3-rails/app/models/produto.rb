dd90bec0125904dd023901835f17ddfcd601edf3
guilherme-mendes <guimendesp12@gmail.com>
2018-11-29 18:42:07 -0200

    Add validations

--- ep3-rails/app/models/produto.rb
+++ ep3-rails/app/models/produto.rb
@@ -1,4 +1,10 @@
 class Produto < ApplicationRecord
-    has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "placeholder.png"
+  has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "placeholder.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  
+  validates :nome, :descrição, :preço, presence: true
+  validates :preço, numericality: { greater_than: 0}
+  validates_attachment_presence :image
+
 end
+  
