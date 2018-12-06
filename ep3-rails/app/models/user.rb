38e79c2e7c806bdfa57b9b64adadfd477a0ba9d3
Lucas Leite <lucasleitemm@hotmail.com>
2018-12-06 00:11:52 -0200

    alterations on admin navbar layout

--- ep3-rails/app/models/user.rb
+++ ep3-rails/app/models/user.rb
@@ -5,10 +5,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  validates :name, presence: true
+  validates :name,:endereco, presence: true
 
   has_many :produtos, dependent: :destroy
   has_many :vendas, class_name: "Pedido", foreign_key: "vendedor_id"
   has_many :compras, class_name: "Pedido", foreign_key: "comprador_id" 
-
 end
